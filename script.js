0972b4df85ad0042b8a54c0b6a2590999c657732
Hazhir Naseri <94365660+hazhir00@users.noreply.github.com>
2024-12-12 12:12:51 +0330

    finished project

---
+++ script.js
@@ -0,0 +1,22 @@
+let toastBox = document.getElementById("toastBox");
+let successMassage = `<i class="fa fa-check-circle" aria-hidden="true"></i> Successfully submitted`;
+let errorMassage = `<i class="fa fa-times" aria-hidden="true"></i> Please fix the error!`;
+let invalidMassage = `<i class="fa fa-exclamation-circle" aria-hidden="true"></i> Invalid input, check again`;
+
+function showToast(massage) {
+    let toast = document.createElement("div");
+    toast.classList.add("toast");
+    toast.innerHTML = massage;
+    toastBox.appendChild(toast);
+
+    if (massage.includes("error")) {
+        toast.classList.add("error");
+    }
+    if (massage.includes("Invalid")) {
+        toast.classList.add("invalid");
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    }, 6000);
+}
